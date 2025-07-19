bbc879ed9b07009883379b790f28ae919f838560
chaitanya-kadali <charankadaliaa@gmail.com>
2025-07-19 22:05:26 +0530

    created components

---
+++ Frontend/src/app/components/Aligners.tsx
@@ -0,0 +1,9 @@
+export default function Aligners(){
+    return(
+        <>
+        <p className="text-2xl">
+            aligners of types fig-2b
+        </p>
+        </>
+    )
+}
---
+++ Frontend/src/app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return(
+        <>
+        <p className="text-2xl">
+            Header
+        </p>
+        </>
+    )
+}
