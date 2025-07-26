a4790bd09b1564ab5661f936dc290045bd21ca15
chaitanya-kadali <charankadaliaa@gmail.com>
2025-07-26 12:12:15 +0530

    style : increased blueheading size

--- Frontend/src/app/components/subcomp/HeadingBlue.tsx
+++ Frontend/src/app/components/subcomp/HeadingBlue.tsx
@@ -8,7 +8,7 @@ const HeadingBlue: FC<HeadingProps> = ({title}) => {
     return(
         <>
         <div className="text-center"> 
-            <h2 className="text-2xl xs:text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold text-blue-600 mb-6 sm:mb-8 relative inline-block group cursor-pointer text-center leading-tight">
+            <h2 className="text-[1.65rem] xs:text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold text-blue-600 mb-6 sm:mb-8 relative inline-block group cursor-pointer text-center leading-tight">
                 <span className="relative z-10 bg-gradient-to-r from-blue-600 via-blue-600 to-blue-600 bg-clip-text text-transparent group-hover:from-blue-600 group-hover:via-blue-800 group-hover:to-blue-600 transition-all duration-700">
                 {title}
                 </span>
